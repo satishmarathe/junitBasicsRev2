9526365b0c16e7fe7f6aebacfe277c9284ccf15d
Satish.Marathe <marathesinpune@gmail.com>
2019-06-10 11:15:27 +1000

    added a Rest endpoint using @GetMapping annotation

--- src/main/java/com/globomantics/productservice/web/MyController.java
+++ src/main/java/com/globomantics/productservice/web/MyController.java
@@ -1,25 +1,40 @@
 package com.globomantics.productservice.web;
 
 import java.math.BigDecimal;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.globomantics.productservice.model.MyOrder;
 import com.globomantics.productservice.model.MyOrderItem;
 import com.globomantics.productservice.model.MyOrderSummary;
 
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@RestController
 public class MyController {
-	
+
+	private static final Logger logger = LogManager.getLogger(MyController.class);
+
 	public MyOrderSummary transform( MyOrder myOrder) {
 		if(null == myOrder) {
 			throw new IllegalArgumentException("myOrder should not be null");
 		}
 		MyOrderSummary myOrderSummaryResult = new MyOrderSummary();
-		
+
 		myOrderSummaryResult.setOrderNumber(myOrder.getOrderNumber());
-		
+
 		/** get the individual items **/
 		List <MyOrderItem> orderItemList = myOrder.getMyOrderItemList();
-		
+
 		int totalItemCount = 0;
 		BigDecimal totalItemPrice = new BigDecimal(0);
 		if(null != orderItemList && orderItemList.size() > 0) {
@@ -29,11 +44,26 @@ public class MyController {
 				System.out.println("myOrderItem.getSellingPrice() = " + myOrderItem.getSellingPrice());
 				System.out.println("myOrderItem.getQuantity() = " + myOrderItem.getQuantity());
 				totalItemPrice = totalItemPrice.add(myOrderItem.getSellingPrice().multiply(new BigDecimal(myOrderItem.getQuantity() )));
-				
+
 			}//for
 		}
 		myOrderSummaryResult.setItemCount(totalItemCount);
 		myOrderSummaryResult.setTotalAmount(totalItemPrice);
 		return myOrderSummaryResult;
+	}
+
+	@GetMapping("/product/{id}")
+	public ResponseEntity<?> getProduct(@PathVariable Integer id) {
+		logger.log(Level.INFO, "inside here ");
+		try {
+			return ResponseEntity
+					.ok()
+					.eTag(Integer.toString(777))
+					.location(new URI("/product/" + 777))
+					.body(null);
+		} catch (URISyntaxException e ) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+		}
+
 	}
 }
